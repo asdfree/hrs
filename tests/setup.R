4eeb42fbe476fb995763620d34a2753496eb1890
Anthony Damico <ajdamico@gmail.com>
2017-10-17 10:38:07 -0400

    lots of time decreases

--- tests/setup.R
+++ tests/setup.R
@@ -9,8 +9,8 @@ hrs_cat <-
 		your_username = my_username , 
 		your_password = my_password )
 
-# sample 75% of the records
-which_records <- sample( seq( nrow( hrs_cat ) ) , round( nrow( hrs_cat ) * 0.75 ) )
+# sample 60% of the records
+which_records <- sample( seq( nrow( hrs_cat ) ) , round( nrow( hrs_cat ) * 0.60 ) )
 
 # always sample the rand a-z stata file
 hrs_cat <- unique( rbind( hrs_cat[ which_records , ] , subset( hrs_cat , grepl( 'rand([a-z]+)stata\\.zip' , file_name ) ) ) )
