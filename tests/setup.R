45aa2522e0e846f0d9f4660d4bae619f4cd94263
Anthony Damico <ajdamico@gmail.com>
2018-01-07 15:09:00 -0500

    hrs needs explicit download block

--- tests/setup.R
+++ tests/setup.R
@@ -1,14 +1,23 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 my_username <- Sys.getenv( "my_username" )
 my_password <- Sys.getenv( "my_password" )
+this_sample_break <- Sys.getenv( "this_sample_break" )
+
 library(lodown)
-lodown( "hrs" , output_dir = file.path( getwd() ) , 
+
+hrs_cat <-
+	get_catalog( "hrs" ,
+		output_dir = file.path( getwd() ) , 
+		your_username = my_username , 
+		your_password = my_password )
+
+record_categories <- ceiling( seq( nrow( hrs_cat ) ) / ceiling( nrow( hrs_cat ) / 3 ) )
+
+hrs_cat <- hrs_cat[ record_categories == this_sample_break , ]
+
+lodown( "hrs" , hrs_cat , 
 	your_username = my_username , 
 	your_password = my_password )
-this_sample_break <- Sys.getenv( "this_sample_break" )
-record_categories <- ceiling( seq( nrow( hrs_cat ) ) / ceiling( nrow( hrs_cat ) / 3 ) )
-hrs_cat <- hrs_cat[ record_categories == this_sample_break , ]
-lodown( "hrs" , hrs_cat )
 if( any( grepl( 'rand([a-z]+)stata\\.zip' , hrs_cat$file_name ) ) ){
 library(lodown)
 # examine all available HRS microdata files
