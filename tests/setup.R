ae32ac8bffa06760d3d6e1d79015e3a8eee98f52
Anthony Damico <ajdamico@gmail.com>
2018-01-13 06:52:50 -0500

    savecache=FALSE and filesize_fun default

--- tests/setup.R
+++ tests/setup.R
@@ -1,4 +1,6 @@
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
+
+options("lodown.cachaca.savecache"=FALSE)
 my_username <- Sys.getenv( "my_username" )
 my_password <- Sys.getenv( "my_password" )
 this_sample_break <- Sys.getenv( "this_sample_break" )
